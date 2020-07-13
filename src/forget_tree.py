bugfix mean nodes calculation

src/forget_tree.py
@@ -13,12 +13,12 @@ np.random.seed(0)
 
 class ForgetHATT(TreeClass):
 
-    node_count_arr = []
 
     def __init__(self, data: str, label: str, forget_percentage: float, delimiter=","):
 
         super().__init__()
 
+        self.node_count_arr = []
         self.counter=0
         self.forgetter = forgetter.Forgetter(data, label, forget_percentage, delimiter=delimiter)
 
@@ -30,28 +30,30 @@ class ForgetHATT(TreeClass):
         media = []#Yaxis
         x_axis = []
 
-        for i in range(0, len(node_count_arr), mean_size):
-            media.append(0)
+        for i in range(0, len(self.node_count_arr), mean_size):
+
             x_axis.append(i + mean_size)
-        
-        for j in range(i, i + mean_size):
-            media[i // mean_size] = media[i // mean_size] + self.node_count_arr[j]
-            media[i // mean_size] = media[i // mean_size] / mean_size
- 
+
+            media.append( sum([ self.node_count_arr[j] for j in filter( lambda x: x < len(self.node_count_arr), range(i, i + mean_size)) ]) / mean_size )
+
         media = [0] + media
         x_axis = [0] + x_axis
+
         return media, x_axis
 
     def mean_nodes_to_csv(self, filepath: str, mean_size = 1000):
 
         with open(filepath, "w") as f:
 
-            writer = csv.DictWriter(f, fieldnames=['x', 'mean number of nodes'])
+            writer = csv.DictWriter(f, fieldnames=['x', 'mean'])
 
             writer.writeheader()
-            for mean, x in zip(self.get_mean_nodes(mean_size)):
 
-                writer.write({ 'x':x, 'mean': mean })
+            media, x_axis = self.get_mean_nodes(mean_size)
+
+            for mean, x in zip(media, x_axis):
+
+                writer.writerow({ 'x':x, 'mean': mean })
 
     def forget_policy(self, X: np.ndarray, Y: np.ndarray):
 
@@ -63,12 +65,12 @@ class ForgetHATT(TreeClass):
             x, y = self.forgetter.next_to_forget()
             super().partial_fit(x, y, -1)
 
-    def partial_fit(self, X: np.ndarray, y: np.ndarray):
-
-        self.node_count_arr.append(self._tree_root.count_nodes()[1])
+    def partial_fit(self, X: np.ndarray, y: np.ndarray, classes=None, sample_weight=None):
 
         # train the model and get metrics based on its predicitons
         super().partial_fit(X, y, classes, sample_weight)
+
+        self.node_count_arr.append(self._tree_root.count_nodes()[1])
 
         if( self.forgetter.forget_percentage != 0 ): self.forget_policy(X, y)
 